efa8cf05df261b7cb03023b0c683a06c23b01386
dipen211 <pateldipen161@gmail.com>
2020-11-30 17:27:13 +0530

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,21 @@ import Create from './pages/Employee/Create';
 import EditCustomer from './pages/Employee/Edit';
 import Navbar from './header/navbar';
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-
+import ErrorBoundary from './HOC/ErrorBoundry';
+import NotFound from "./pages/NotFound";
 class App extends React.Component<RouteComponentProps<any>> {
   public render() {
     return (
       <div className="App">
+        <ErrorBoundary>
         <Navbar />
         <Switch>
           <Route path={'/'} exact component={Employees} />
           <Route path={'/create/:id'} exact component={Create} />
           <Route path={'/edit/:id'} exact component={EditCustomer} />
+          <Route component={NotFound} />
         </Switch>
+        </ErrorBoundary>
       </div>
     );
   }
